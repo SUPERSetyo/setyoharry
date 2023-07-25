7cc1d4c7989cb1d4a645d75574f803e23933e4b5
setyoharry <akuntumbal05pes@gmail.com>
2023-07-25 18:21:15 +0700

    push pertama saya

---
+++ pertemuan5_copy/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Link</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <a href="tambah_mahasiswa.php">Tambah Mahasiswa</a>
+    <table border="1">
+        <tr>
+            <th>No</th>
+            <th>NPM</th>
+            <th>Nama Mahasiswa</th>
+            <th>Jenis Kelamin</th>
+            <th>Tanggal Lahir</th>
+            <th>Alamat</th>
+            <th>Program Studi</th>
+            <th>Opsi</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>21091010</td>
+            <td>Reza</td>
+            <td>Pria</td>
+            <td>01/05/2001</td>
+            <td>Medan</td>
+            <td>Sistem Informasi</td>
+            <td><a href="edit_mahasiswa.php">Edit</a></td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>21091011</td>
+            <td>Raja</td>
+            <td>Pria</td>
+            <td>01/07/2002</td>
+            <td>Siborong-borong</td>
+            <td>Teknologi Informasi</td>
+            <td><a href="edit_mahasiswa.php">Edit</a></td>
+        </tr>
+    </table>
+</body>
+</html>
